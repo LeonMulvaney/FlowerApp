6c435571e1baa2ba43a29109bcc118ce9ad7e1d5
LeonMulvaney <leonmul96@gmail.com>
2016-10-28 16:30:53 +0100

    Rounded Buttons Added - NEW XML file in drawable to do this

--- app/src/main/java/com/leondesigns/flowerapp/MainActivity.java
+++ app/src/main/java/com/leondesigns/flowerapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void viewHome(View view){
