6f90ca3a69f3997ba9be4fcecb3e78833c417efb
Aaron Healy <healy464@hotmail.com>
2016-10-29 00:11:07 +0100

    database and register page

--- app/src/main/java/com/leondesigns/flowerapp/MainActivity.java
+++ app/src/main/java/com/leondesigns/flowerapp/MainActivity.java
@@ -4,20 +4,47 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailInput;
+    EditText nameInput;
+    EditText phoneInput;
+    EditText addressInput;
+
+    DBhandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        phoneInput = (EditText) findViewById(R.id.phoneInput);
+        addressInput = (EditText) findViewById(R.id.addressInput);
+
+        dbHandler = new DBhandler(this, null, null, 1);
+    }
+    //add stuff to db
+    public void registerButtonClicked(){
+        RegisterUser registeruser = new RegisterUser(emailInput.getText().toString(), nameInput.getText().toString(), phoneInput.getText().toString(), addressInput.getText().toString());
+        dbHandler.addUser(registeruser);
     }
 
+
+
+
+//login button goes to homepage though
     public void viewHome(View view){
         Intent viewHome = new Intent(this,home.class);
         startActivity(viewHome);
+    }
+
+    public void viewRegisterUser(View view){
+        Intent viewRegisterUser = new Intent(this, RegisterUser.class);
+        startActivity(viewRegisterUser);
     }
 
 
--- app/src/main/java/com/leondesigns/flowerapp/catalogue.java
+++ app/src/main/java/com/leondesigns/flowerapp/catalogue.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class catalogue extends AppCompatActivity {
 
@@ -11,10 +16,32 @@ public class catalogue extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogue);
+
+        String[] cat = {"Old Rose", "Red Rose","Lucky Star","Allure", "Old Rose", "Red Rose","Lucky Star","Allure", "Old Rose", "Red Rose","Lucky Star","Allure"};
+
+        ListAdapter listAdapter = new customAdapter(this, cat);
+
+        ListView CatalogueListView = (ListView)findViewById(R.id.CatalogueListView);
+        CatalogueListView.setAdapter(listAdapter);
+
+        CatalogueListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> perant, View view, int position, long id) {
+                        String cat = String.valueOf(perant.getItemAtPosition(position));
+                        Toast.makeText(catalogue.this, cat, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 
     public void viewHome(View view){
         Intent viewHome = new Intent(this,home.class);
         startActivity(viewHome);
     }
+
+
+
+
+
 }
